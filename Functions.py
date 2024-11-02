cb7c91341e4bdc201f0f75ab2d5f209bc6931926
haskir <haskird2@gmail.com>
2024-11-02 16:15:09 +0300

    cache

--- Functions.py
+++ Functions.py
@@ -1,5 +1,8 @@
 import os.path
 import random
+from pprint import pprint
+
+import aiofiles
 import bs4
 import xml.etree.ElementTree as ET
 from httpx import AsyncClient
@@ -20,11 +23,11 @@ async def update_list() -> bool:
             return False
 
 
-def read_pastas_file() -> None | list:
+async def read_pastas_file() -> None | list:
     path = r"./pastas.txt"
     if not os.path.exists(path):
         print("Didn't find file with pastas")
-        if not update_list():
+        if not await update_list():
             print("Error in read_pastas_file")
             return None
         print("Got new file")
@@ -32,24 +35,41 @@ def read_pastas_file() -> None | list:
         return file.readline().split(" ")
 
 
+async def download_page(url: str) -> str | None:
+    async with AsyncClient() as client:
+        response = await client.get(url)
+        if response.status_code != 200:
+            return ""
+        return response.text
+
+
+def parse_pasta(text: str) -> str:
+    try:
+        soup = bs4.BeautifulSoup(text, "html.parser")
+        PastaElement = soup.find("h2", string="Текст копипасты")
+        buttons_start_index = PastaElement.next_element.next_element.get_text().index("content_copy")
+        return PastaElement.next_element.next_element.get_text()[0:buttons_start_index]
+    except BaseException as e:
+        print(e)
+        return "Не смог запарсить пасту :("
+
+
 async def get_pasta() -> str | bool:
-    url = r"https://copypastas.ru/"
-    available_pastas = read_pastas_file()
+    url = r"https://copypastas.ru/copypasta"
+    available_pastas = await read_pastas_file()
+    random_id: int = random.choice(available_pastas)
+    cached_pasta = await cache.get(random_id)
+    if cached_pasta:
+        return cached_pasta
     if not available_pastas:
         await update_list()
         return await get_pasta()
-    else:
-        try:
-            async with AsyncClient() as client:
-                response = await client.get(f"{url}copypasta/{random.choice(available_pastas)}")
-                if response.status_code != 200:
-                    return "Не смогу получить пасту :("
-                soup = bs4.BeautifulSoup(response.text, "html.parser")
-                PastaElement = soup.find("h2", string="Текст копипасты")
-                buttons_start_index = PastaElement.next_element.next_element.get_text().index("content_copy")
-                return PastaElement.next_element.next_element.get_text()[0:buttons_start_index]
-        except BaseException as e:
-            return "Не смог запарсить пасту :("
+    response_text = await download_page(f'{url}/{random_id}/')
+    if not response_text:
+        return "Не смогу получить пасту :("
+    parsed = parse_pasta(response_text)
+    await cache.save(random_id, parsed)
+    return parsed
 
 
 def add_user_to_file(user: str | int):
@@ -80,3 +100,34 @@ def remove_user(user: str | int):
         users.remove(str(user))
         with open(path, "w") as file:
             [file.write(user.rstrip() + ";") for user in users]
+
+
+class PastaCache:
+    def __init__(self, path: str = r'./pastas/'):
+        self.cache: dict[int, str] = dict()
+        self.path = path
+        if not os.path.exists(path):
+            os.mkdir(path)
+        else:
+            for pasta_txt in os.listdir(path):
+                with open(f'{self.path}{pasta_txt}') as file:
+                    self.cache[int(pasta_txt.split(".")[0])] = file.read()
+
+    async def get(self, ID: int) -> str:
+        if ID in self.cache:
+            return self.cache[ID]
+        if not os.path.exists(f'{self.path}{ID}.txt'):
+            return ""
+        async with aiofiles.open(f'{self.path}{ID}.txt') as file:
+            pasta = await file.read()
+            if pasta:
+                self.cache[ID] = pasta
+            return pasta
+
+    async def save(self, ID: int, pasta: str) -> None:
+        self.cache[ID] = pasta
+        async with aiofiles.open(f'{self.path}{ID}.txt', "w") as file:
+            await file.write(pasta)
+
+
+cache = PastaCache()
