3d1ab41a34111da91c6699aded6b8bd2873b28bd
LapkiNaStol <haskird2@gmail.com>
2025-11-09 18:55:21 +0300

    чисто читаемость поднял

--- src/routers/admin_router.py
+++ src/routers/admin_router.py
@@ -8,24 +8,27 @@ from src.helpers import ScheduleSender
 __all__ = ["configure_admin_router"]
 
 
+async def start_mailing(message: Message, sender: FromDishka[ScheduleSender]):
+    if sender.enabled:
+        await message.answer(text="Рассылка уже была включена ранее")
+    else:
+        sender.enabled = True
+        await message.answer(text="Включил рассылку")
+
+
+async def stop_mailing(message: Message, sender: FromDishka[ScheduleSender]):
+    if not sender.enabled:
+        await message.answer(text="Рассылка неактивна")
+    else:
+        sender.enabled = False
+        await message.answer(text="Выключил рассылку")
+
+
 def configure_admin_router(admin_id: int) -> Router:
     admin_router = Router(name="Admin router")
     admin_router.message.filter(F.from_user.id == admin_id)
 
-    @admin_router.message(Command(commands=["start_mailing"]))
-    async def start_mailing(message: Message, sender: FromDishka[ScheduleSender]):
-        if sender.enabled:
-            await message.answer(text="Рассылка уже была включена ранее")
-        else:
-            sender.enabled = True
-            await message.answer(text="Включил рассылку")
-
-    @admin_router.message(Command(commands=["stop_mailing"]))
-    async def stop_mailing(message: Message, sender: FromDishka[ScheduleSender]):
-        if not sender.enabled:
-            await message.answer(text="Рассылка неактивна")
-        else:
-            sender.enabled = False
-            await message.answer(text="Выключил рассылку")
+    admin_router.message.register(start_mailing, Command(commands=["start_mailing"]))
+    admin_router.message.register(stop_mailing, Command(commands=["stop_mailing"]))
 
     return admin_router
